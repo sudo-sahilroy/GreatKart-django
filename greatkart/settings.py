4db61a04608efe27255bdcf25e3a6e242d97b987
Sahil Roy <sahilroy2001.sr@gmail.com>
2022-12-15 19:53:59 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -135,5 +135,13 @@ MEDIA_ROOT=BASE_DIR /'media'
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
-    
+
 }
+
+# SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sahilroy2001.sr@gmail.com'
+EMAIL_HOST_PASSWORD = 'bxflersxbqpasqtw'
